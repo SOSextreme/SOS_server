49c4b8b688d504a4815ada8c46e587dedac226b4
jingyue-liu <jl4753@columbia.edu>
2017-02-02 13:25:08 -0500

    quick fix

--- server/video-processor.js
+++ server/video-processor.js
@@ -160,7 +160,7 @@ module.exports = function (app) {
 					ws.send(data["fbid"].toString());
 					
 					client.calls.create({                                  //make outbound call
-						url: "http://"+req.headers.host+"/h/"+fbid",
+						url: "http://"+req.headers.host+"/h/"+fbid,
 						to: toNumber,
 						from: fromNumber
 					}, function(err, call) {
